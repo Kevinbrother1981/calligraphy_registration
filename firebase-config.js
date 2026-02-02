8ae69530020654fc6ecf9c3a502dde3b90ee2e25
Kevin Wu <junichoon@gmail.com>
2026-02-03 00:14:58 +0800

    Update Firebase config and ensure region URL is correct

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,4 @@
 // Firebase Configuration
-// Keys provided by user for project: creat-eac37
 const firebaseConfig = {
     apiKey: "AIzaSyArkHnEyRb8JbMnGzzMvw6hloPONcAIe1Q",
     authDomain: "creat-eac37.firebaseapp.com",
@@ -7,8 +6,8 @@ const firebaseConfig = {
     storageBucket: "creat-eac37.firebasestorage.app",
     messagingSenderId: "151348049220",
     appId: "1:151348049220:web:99803f2bc1953e7e559311",
-    // Inferred Database URL for this project
-    databaseURL: "https://creat-eac37-default-rtdb.asia-southeast1.firebasedatabase.app/",
+    // Database URL is required for Realtime Database, especially for non-US regions
+    databaseURL: "https://creat-eac37-default-rtdb.asia-southeast1.firebasedatabase.app/"
 };
 
 // Initialize Firebase (Compat version)
